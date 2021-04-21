f46fd01e7d67808ca56251516b47d65b023f2865
joaozandona <joaoricardozandona@hotmail.com>
2021-04-21 17:47:28 -0300

    Ajeitei o tamanho das imagens das thumbails dos outros pdcasts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,7 +71,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
               {allEpisodes.map(episode => {
                 return(
                   <tr key={episode.id}>
-                    <td>
+                    <td style={{ width: 72 }}>
                       <Image width={120} height={120} src={episode.thumbnail} alt={episode.title} objectFit="cover" />
                     </td>
                     <td>
