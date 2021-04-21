961e237d517a7151e5630a74920647e6a9fbb274
joaozandona <joaoricardozandona@hotmail.com>
2021-04-21 18:40:06 -0300

    Criei a página estática dinâmica de cada podcast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 import Image from 'next/image';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import Link from 'next/link'
 
 import styles from './home.module.scss'
 
@@ -11,7 +12,6 @@ type Episode = {
   id: string;
   title: string;
   thumbnail: string;
-  description: string;
   members: string;
   duration: number;
   durationAsString: string;
@@ -37,7 +37,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
 
                 <div className={styles.episodeDetails}>
-                  <a href="#">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
                   <p>{episode.members}</p>
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
@@ -58,14 +60,14 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
           <table cellSpacing={0}>
 
             <thead>
-
-              <th></th>
-              <th>Podcast</th>
-              <th>Integrantes</th>
-              <th>Data</th>
-              <th>Duração</th>
-              <th></th>
-
+              <tr>
+                <th></th>
+                <th>Podcast</th>
+                <th>Integrantes</th>
+                <th>Data</th>
+                <th>Duração</th>
+                <th></th>
+              </tr>
             </thead>
             <tbody>
               {allEpisodes.map(episode => {
@@ -75,7 +77,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                       <Image width={120} height={120} src={episode.thumbnail} alt={episode.title} objectFit="cover" />
                     </td>
                     <td>
-                      <a href="#">{episode.title}</a>
+                      <Link href={`/episodes/${episode.id}`}>
+                        <a>{episode.title}</a>
+                        </Link>
                     </td>
                     <td>{episode.members}</td>
                     <td style={{width: 100}}>{episode.publishedAt}</td>
@@ -114,9 +118,8 @@ export const getStaticProps: GetStaticProps = async () => {
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-      description: episode.description,
       url: episode.file.url,
-    }
+    };
   })
 
   const latestEpisodes = episodes.slice(0, 2);
